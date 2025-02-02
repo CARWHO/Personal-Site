9b709d3483738fb6db0f5baa5dfe3d3f3f713e17
CARWHO <kahuhutton.business@gmail.com>
2025-02-02 15:33:28 +1300

    fix: Adjust portfolio graph column margin positioning

--- src/app/HomeContent.tsx
+++ src/app/HomeContent.tsx
@@ -53,7 +53,7 @@ export default function HomeContent() {
             </Text>
           </RevealFx>
         </Column>
-        <Column style={{ marginTop: "-100px", marginLeft: "30%" }}>
+        <Column style={{ marginTop: "-100px", marginLeft: "-50%" }}>
           <PortfolioGraph />
         </Column>
       </Flex>
